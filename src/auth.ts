f39efdef13eabb5c246a7e9cd4632cb3a669f9b6
Jordi <jordism91@gmail.com>
2024-08-01 10:03:09 +0200

    more logs

--- src/auth.ts
+++ src/auth.ts
@@ -16,11 +16,14 @@ const figmaProvider: Provider = {
   token: {
     url: "https://www.figma.com/api/oauth/token",
     async request(context: any) {
+      console.log(context);
       const provider = context.provider;
+      console.log(provider);
       const res = await fetch(
         `https://www.figma.com/api/oauth/token?client_id=${provider.clientId}&client_secret=${provider.clientSecret}&redirect_uri=${provider.callbackUrl}&code=${context.params.code}&grant_type=authorization_code`,
         { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" } }
       );
+      console.log(res);
       const json = await res.json();
       console.log(json);
       return { tokens: json };
